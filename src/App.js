9f90b2bc454951a9640d0540d13fc5c8e070f738
vlassov_i <vlasov.i.88@gmail.com>
2021-07-28 22:03:35 +0500

    Lesson2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
-// import logo from './logo.svg';
-// import './App.css';
+import "./App.css";
+import "./components/MessageList/messageList.css";
+import { useRef, useEffect, useState, useCallback } from "react";
+import { Message } from "./components/Message/message";
+import { MessageList } from "./components/MessageList/messageList";
+import { Form } from "./components/Form/form";
+import { AUTHORS } from "./constants";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  const heading = "Чат REACT";
 
-// export default App;
-import './App.css';
-import Message from './components/Message';
+  const [messages, setMessages] = useState([]);
 
-const myMessage = "My message text!";
+  const handleSendMessage = useCallback(
+    (newMessage) => {
+      setMessages([...messages, newMessage]);
+      console.log(newMessage);
+    },
+    [messages]
+  );
 
-function App(props) {
- return (
-   <div className="App">
-     <header className="App-header">
-       My First React App
-       <h3>Hello, {props.name}</h3>
-       <Message message={myMessage} />
-     </header>
-   </div>
- );
+  useEffect(() => {
+    if (
+      !messages.length ||
+      messages[messages.length - 1].author === AUTHORS.robot
+    ) {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      const newMessage = {
+        text: "Я робот!",
+        author: AUTHORS.robot,
+        id: Date.now(),
+      };
+
+      setMessages([...messages, newMessage]);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, [messages]);
+
+  return (
+    <div className="App-header">
+
+      <h1>{ heading }</h1>
+      <div>
+        <Form onSendMessage={handleSendMessage} />
+        <div className="MessageList">
+          <MessageList messages={messages} />
+        </div>
+      </div>
+    </div>
+    
+  );
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const myName = "WORLD";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={myName} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
